0e701c90c9f01fbe73c54cbf21a9927f734b5e00
besanger321 <43681296+besanger321@users.noreply.github.com>
2021-09-17 00:24:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+client.on("message", message => {
+  if (message.author.bot) return;
+
+  let content = message.content.split(" ");
+  let command = content[0];
+  let args = content.slice(1);
+
+  if (message.content.startsWith(prefix)) {
+      try {
+          let commandFile = require(`./commands/${command.slice(prefix.length)}.js`)
+          commandFile.execute(client, message, args);
+      } catch (e) {
+          console.warn(`Erreur : ${e}`);
+          return;
+      }
+  }
+});
+
+client.on('message', message => {
+  if(message.author.bot) return;
+  if(message.channel.type==="dm"||message.channel.type==="group") return message.channel.send("Je ne peux vous répondre.");
+});
+  
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.TOKEN);
