46b8abe6b0092dabd6c8dc6d7fb4658d35cd3208
besanger321 <43681296+besanger321@users.noreply.github.com>
2021-09-17 01:08:18 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'kick du serveur',
 	execute(client, message, args) {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission.");
-message.channel.bulkDelete(6)
+message.channel.bulkDelete(7)
 console.log(`6 message on était retiré par ${message.author.tag}`);
         message.channel.send("6 messages en moins");
     }
