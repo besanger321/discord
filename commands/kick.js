377908742835a32cd69c2db8de702b87af826c97
besanger321 <43681296+besanger321@users.noreply.github.com>
2021-09-23 09:05:14 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -7,7 +7,8 @@ module.exports = {
         if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("tu n'a pas les permissions");
         if(!message.mentions.users.first()) return message.channel.send("mention inexistante ou éroner");
           console.log(`${user.tag} a été kick par ${message.author.tag}`);  
-          member.kick();
+          member.send("Tu a était kick par le staff.");
+	  member.kick();
           message.channel.send(`A plus dans le bus ${user.tag}`);
         }
     };
