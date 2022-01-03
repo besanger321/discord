88729001e920c54afde3ed308c354f47e18b7aaf
besanger321 <43681296+besanger321@users.noreply.github.com>
2022-01-03 21:54:12 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -4,5 +4,6 @@ module.exports = {
 	execute(client, message, args) {
             const timetTaken = Date.now() - message.createdTimestamp;
             message.channel.send(`Pong! la latence est de ${timetTaken} ms.`)
+			console.log(`${message.author.tag} a demander le ping qui est de ${timetTaken}`)
           },
 		};
